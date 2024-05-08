e9ebd23def5f801678eed63c09cc931145f40769
JuanCruz <juancruzgenovese22@gmail.com>
2024-05-08 19:54:48 -0300

    Link nuevo

--- src/components/Landing/FirstView/FirstView.jsx
+++ src/components/Landing/FirstView/FirstView.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import style from "./FirstView.module.css"
 
 const FirstView = () => {
@@ -12,7 +13,10 @@ const FirstView = () => {
             <img src="/AnurApp2.png" className={style.logo}/>
             <p className={style.Description}>App gratuita que permite conocer e identificar las diferentes especies de ranas y sapos presentes en la provincia de Jujuy. </p>
             <p className={style.txt}>¡Conocé la diversidad de anfibios de Jujuy!</p>
-            <button className={style.DownloadButton}> DESCARGAR </button>
+            <Link to="https://biodiversidadjujuy.com.ar/" className={style.Link}>
+              <button className={style.DownloadButton}> DESCARGAR </button>
+            </Link>
+            
   
         </div>     
 
--- src/views/MasInfo/MasInfo.jsx
+++ src/views/MasInfo/MasInfo.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import style from "./MasInfo.module.css"
 
 const MasInfo = () => {
+
+  useEffect (() => {
+    window.scrollTo(0,0)
+  },[])
+  
   return (
     <div className={style.cont}>
       <img src="/MasInfoBG1.jpg" className={style.BG}/>
